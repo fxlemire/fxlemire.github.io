e838322b78e01268c328a1d3c7d172f27cced466
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2018-02-01 08:57:35 -0500

    fix favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -19,7 +19,7 @@ const TemplateWrapper = ({ children }) => (
         href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
       />
       <title>{siteMetadata.title}</title>
-      <link rel="icon" href="../assets/favicon.png" type="image/x-icon"/>
+      <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
     </Helmet>
     {children()}
   </div>
